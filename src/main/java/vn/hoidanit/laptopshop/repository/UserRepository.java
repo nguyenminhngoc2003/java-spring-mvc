847139bf5cd7a8c72b55719057074ef4b6606342
nguyenminhngoc2003 <minhngoc2772003@gmail.com>
2025-04-07 13:47:04 +0700

    module user basic

---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package vn.hoidanit.laptopshop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.hoidanit.laptopshop.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User hoidanit);
+
+    List<User> findByEmail(String email);
+
+    List<User> findAll();
+
+    User findById(long id);
+
+    void deleteById(long id);
+}
---
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -0,0 +1,39 @@
+package vn.hoidanit.laptopshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public User handleSaveUser(User user) {
+        User ngoc = this.userRepository.save(user);
+        System.out.println(ngoc);
+        return ngoc;
+    }
+
+    public void deleteAUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+}
